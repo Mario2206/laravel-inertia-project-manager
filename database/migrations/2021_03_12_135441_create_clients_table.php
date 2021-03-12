67e15575513349256e589bbef12789fcce22bc39
Mario2206 <mathieu220601@gmail.com>
2021-03-12 15:16:29 +0100

    feat: add clients table

---
+++ database/migrations/2021_03_12_135441_create_clients_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("name");
+            $table->longText("description");
+            $table->string("business_name");
+            $table->string("legal_status");
+            $table->float("capital");
+            $table->string("siret");
+            $table->string("naf_code");
+            $table->string("country");
+            $table->text("adress");
+            $table->string("postal_code");
+            $table->string("city");
+            $table->foreignId("user_id")->constrained("users");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
